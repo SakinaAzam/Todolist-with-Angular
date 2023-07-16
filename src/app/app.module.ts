05e98986f60a3c743ab886d981428c5b380a6288
SAKINA AZAM <205117adp@gmail.com>
2023-07-16 23:20:07 +0500

    initiall commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './MyComponent/todo/todo.component';
+import { TodoItemComponent } from './MyComponent/todo-item/todo-item.component';
+import { AddTodoComponent } from './Mycomponent/add-todo/add-todo.component';
+import { AboutComponent } from './MyComponent/about/about.component';
+import { HomeComponent } from './MyComponent/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoItemComponent,
+    AddTodoComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
